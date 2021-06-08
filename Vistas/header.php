ecb4d5ddbf013b64c8c4e24cbaab39d5bd6b2db4
mr01robot <toshiro.kenny@gmail.com>
2021-06-07 20:33:31 -0500

    header and footer

--- Vistas/header.php
+++ Vistas/header.php
@@ -1,16 +1,24 @@
-    <header class="header">
-        <div class="container logo-nav-container">
+<header class="header">
+        <div class="container logo-image">
             <a href="index.php" class="logo">
                 <img src="Vistas\images\Logo_Contigo_Voy.png" alt=""/>
             </a>
+            <span class="login-icon">
+                <a href="login.php"><img src="Vistas\images\Icono_usuario.png" alt=""/></a>
+            </span>
+            <a href="login.php" class="login-user">Iniciar sesión</a>
+        </div>
+        <div class="container logo-nav-container">
             <span class="menu-icon"><img src="Vistas\images\Icono_menu.png" alt=""/></span>
             <nav class="navigation">
                 <ul>
                     <li><a href="#">Inicio</a></li>
-                    <li><a href="#">Nosotros</a></li>
-                    <li><a href="#">Servicios</a></li>
-                    <li><a href="#">Contacto</a></li>
-                    <li><input type="submit" class="login" name="login" value="Iniciar sesión"/></li>
+                    <li><a href="#">Nuestros Servicios</a></li>
+                    <li><a href="#">Talleres</a></li>
+                    <li><a href="#">Especialistas</a></li>
+                    <li><a href="#">Modalides de trabajo</a></li>
+                    <li><a href="#">Contáctanos</a></li>
+                    <li><a href="#">Material para ti</a></li>
                 </ul>
             </nav>
         </div>
